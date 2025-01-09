b6a36f0c6f8dc9d00380aff06638b8618366f7b7
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2025-01-09 12:21:19 +0530

    Update system_call.c

--- EXP2/system_call.c
+++ EXP2/system_call.c
@@ -5,3 +5,8 @@ int main() {
     char *args[] = {"./sample", NULL};
     execv(args[0], args);
 }
+
+// csea2@sjcet-WIV59355-0116:~/AmalJoy$ gcc sample.c -o sample
+// csea2@sjcet-WIV59355-0116:~/AmalJoy$ gcc system_call.c
+// csea2@sjcet-WIV59355-0116:~/AmalJoy$ ./a.out
+// Running sample.c using systemcall.c
